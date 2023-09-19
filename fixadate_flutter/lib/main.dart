e6590e891795e9838b235cdd0bbfbcd326e4f037
sungjun4403 <geulligu89@naver.com>
2023-09-19 13:50:58 +0900

    .

--- fixadate_flutter/lib/main.dart
+++ fixadate_flutter/lib/main.dart
@@ -5,20 +5,20 @@ void main() {
   runApp(BackgroundColor());
 }
 
-// class BackgroundColor extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: 150,
-//       height: 100,
-//       decoration: const BoxDecoration(
-//         color: Color.fromRGBO(19, 19, 19, 1.0),
-//       ),
-//     );
-//
-//   }
-// }
+class BackgroundColor extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 100,
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(19, 19, 19, 1.0),
+      ),
+    );
+
+  }
+}
 
 
 
